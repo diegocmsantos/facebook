d8daebc41b438f5fc91177cfda15313c125097da
Diego Maia <diegocmsantos@gmail.com>
2012-08-21 16:14:41 -0300

    Fixing getBackFacebook url

--- app/controllers/social_medias_controller.rb
+++ app/controllers/social_medias_controller.rb
@@ -11,25 +11,18 @@ class SocialMediasController < ApplicationController
 		@code = params[:code]
 		@access_token = params[:access_token]
 	    if @code && !@access_token
-			# Handle a successful save.
-			flash[:success] = "ok!"
-			@ret = @code
-			#redirect_to "https://graph.facebook.com/oauth/access_token?client_id=221499501259361&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Ffacebook%2Fgetaccesstoken&client_secret=b978dc60ab73d27ec38ecc78931a6f72&code=" + @code
 
 			url = URI.parse("https://graph.facebook.com/oauth/access_token?client_id=221499501259361&redirect_uri=http%3A%2F%2Ffloating-meadow-2521.herokuapp.com%2Fsocial_medias%2Fgetaccesstoken&client_secret=81c8de19c6f7e93d771dcf1dfd0d0eae&code=" + @code)
-			#url = URI.parse("http://google.com")
 			http = Net::HTTP.new(url.host, url.port)
 			http.verify_mode = OpenSSL::SSL::VERIFY_NONE
 			http.use_ssl = true
 
-			#req = Net::HTTP::Get.new(url.request_uri)
 			res = http.request_get(url.path + '?' + url.query)
 			@retorno = res.body
 
 			at = res.body.split "&"
 			@access_token = at.first.delete "access_token="
 			
-	    	@ret = @access_token
 	    	render "face"
 	    end
 
